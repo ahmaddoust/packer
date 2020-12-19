49258e779fd8d64583e704e3b4ebac9c30cd1d17
maddusahot <ahmaddoust@gmail.com>
2020-12-19 14:56:43 +0100

    packer

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+sudo yum -y update
+curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | bash
+source ~/.bashrc
+nvm install $NODE_VERSION
+nvm use $NODE_VERSION
+cd /home/ec2-user/test-server
+npm i
+npm i forever -g 
---
+++ test-server/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const axios = require('axios');
+const port = 3000;
+const app = express();
+
+
+let num = Math.floor(Math.random() * 10000);
+const version = 2 //process.env.VERSION;
+
+app.get('/v2', (request, response) => {
+  console.log(`From server ${num} Received request for URL: ${request.url} version ${version}`)
+  response.writeHead(200);
+  response.end(`Hello World! from server ${num} version ${version}`);
+
+})
+
+app.get('/health', (request, response) => {
+  console.log(`Health check instance ${num} Received request for URL: ${request.url} version ${version}`)
+  response.writeHead(200);
+  response.end(`I am healthy! from server ${num} version ${version}`);
+
+})
+
+
+app.get('/readiness', (request, response) => {
+  console.log(`Readiness check instance ${num} Received request for URL: ${request.url} version ${version}`)
+
+  try {
+    response.writeHead(200);
+    response.end(`I am ready! from server ${num} version ${version}`);
+  } catch (e) {
+    console.log('error on size > 0')
+  }
+});
+
+
+app.listen(port, (err) => {
+  console.log("started server");
+  if (err)
+    return console.log(`something happened on startup with text`);
+});
