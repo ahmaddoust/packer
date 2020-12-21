7e4b5685eee7a4c4db6ecaa3515ee23780d162ef
Ahmad Doust <74048386+maddusahot@users.noreply.github.com>
2020-12-21 14:34:34 +0100

    Update server.js

--- test-server/server.js
+++ test-server/server.js
@@ -5,9 +5,9 @@ const app = express();
 
 
 let num = Math.floor(Math.random() * 10000);
-const version = 2 //process.env.VERSION;
+const version = 1 //process.env.VERSION;
 
-app.get('/v2', (request, response) => {
+app.get('/v1', (request, response) => {
   console.log(`From server ${num} Received request for URL: ${request.url} version ${version}`)
   response.writeHead(200);
   response.end(`Hello World! from server ${num} version ${version}`);
